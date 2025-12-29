45bc4c4f2e645eb70110ead425b52929c746f97b
hojat72elect <hojat72elect@gmail.com>
2025-12-28 22:03:35 -0500

    Added dependencies for LWJGL.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,25 @@ plugins {
 group = "com.kerman"
 version = "0.0.1"
 
+val lwjglVersion = "3.4.0-SNAPSHOT"
+val lwjglNatives = "natives-windows"
+
 repositories {
     mavenCentral()
+    maven("https://central.sonatype.com/repository/maven-snapshots")
 }
 
 dependencies {
+
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+    implementation("org.lwjgl:lwjgl")
+    implementation("org.lwjgl:lwjgl-opengl")
+
+    // natives
+    implementation ("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+
+    // test
     testImplementation(kotlin("test"))
 }
 
