85e55200acd983d6d310e5c67ecf0e5a9f7b8921
hojat72elect <hojat72elect@gmail.com>
2025-12-31 02:21:08 -0500

    Added specifications for the most important versions of OpenGL ES API.

--- src/main/kotlin/com/kerman/core/Graphics.kt
+++ src/main/kotlin/com/kerman/core/Graphics.kt
@@ -1,5 +1,10 @@
 package com.kerman.core
 
+/**
+ * This interface was inspired by "com.badlogic.gdx.Graphics".
+ *
+ * It mostly, just encapsulates the communications with GPU.
+ */
 interface Graphics {
 
 }
