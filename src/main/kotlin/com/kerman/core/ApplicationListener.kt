d50b015316ddedde476db078b8f5c8125c07cc1a
hojat72elect <hojat72elect@gmail.com>
2026-01-01 02:13:08 -0500

    ApplicationListener.kt

---
+++ src/main/kotlin/com/kerman/core/ApplicationListener.kt
@@ -0,0 +1,49 @@
+package com.kerman.core
+
+/**
+ * Info : This interface was inspired by "com.badlogic.gdx.ApplicationListener".
+ *
+ * An `ApplicationListener` is called when the [Application] is created, resumed, rendering, paused or destroyed.
+ * All methods are called in a thread that has the OpenGL context current. You can thus safely create and manipulate graphics
+ * resources.
+ *
+ * The `ApplicationListener` interface follows the standard Android activity life-cycle and is emulated on the desktop
+ * accordingly.
+ */
+interface ApplicationListener {
+
+    /**
+     * Called when the [Application] is first created.
+     */
+    fun create()
+
+    /**
+     * Called when the [Application] is resized. This can happen at any point during a non-paused state but will never
+     * happen before a call to [.create].
+     *
+     * @param width  the new width in pixels
+     * @param height the new height in pixels
+     */
+    fun resize(width: Int, height: Int)
+
+    /**
+     * Called when the [Application] should render itself.
+     */
+    fun render()
+
+    /**
+     * Called when the [Application] is paused, usually when it's not active or visible on-screen. An Application is also
+     * paused before it is destroyed.
+     */
+    fun pause()
+
+    /**
+     * Called when the [Application] is resumed from a paused state, usually when it regains focus.
+     */
+    fun resume()
+
+    /**
+     * Called when the [Application] is destroyed. Preceded by a call to [.pause].
+     */
+    fun dispose()
+}
