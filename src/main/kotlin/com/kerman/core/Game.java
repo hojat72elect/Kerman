b81268588de7ce542c0e99443c2ec4badece3287
hojat72elect <hojat72elect@gmail.com>
2026-01-01 02:36:39 -0500

    Game.java

---
+++ src/main/kotlin/com/kerman/core/Game.java
@@ -0,0 +1,62 @@
+package com.kerman.core;
+
+/**
+ * Info : This abstract class was inspired by "com.badlogic.gdx.Game".
+ * <p>
+ * An {@link ApplicationListener} that delegates to a {@link Screen}. This allows an application to easily have multiple screens.
+ * </p>
+ * <p>
+ * Screens are not disposed automatically. You must handle whether you want to keep screens around or dispose of them when another
+ * screen is set.
+ * </p>
+ */
+public abstract class Game implements ApplicationListener {
+    protected Screen screen;
+
+    @Override
+    public void dispose() {
+        if (screen != null) screen.hide();
+    }
+
+    @Override
+    public void pause() {
+        if (screen != null) screen.pause();
+    }
+
+    @Override
+    public void resume() {
+        if (screen != null) screen.resume();
+    }
+
+    @Override
+    public void render() {
+        if (screen != null) screen.render(Kerman.graphics.getDeltaTime());
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        if (screen != null) screen.resize(width, height);
+    }
+
+    /**
+     * @return the currently active {@link Screen}.
+     */
+    public Screen getScreen() {
+        return screen;
+    }
+
+    /**
+     * Sets the current screen. {@link Screen#hide()} is called on any old screen, and {@link Screen#show()} is called on the new
+     * screen, if any.
+     *
+     * @param screen may be {@code null}
+     */
+    public void setScreen(Screen screen) {
+        if (this.screen != null) this.screen.hide();
+        this.screen = screen;
+        if (this.screen != null) {
+            this.screen.show();
+            this.screen.resize(Kerman.graphics.getWidth(), Kerman.graphics.getHeight());
+        }
+    }
+}
