e3e3a85a2f3fc3d2fccede16714ebe324eacbba8
hojat72elect <hojat72elect@gmail.com>
2026-01-03 08:17:28 -0500

    removed ApplicationType.kt

--- src/main/kotlin/com/kerman/core/ApplicationType.kt
+++
@@ -1,8 +0,0 @@
-package com.kerman.core
-
-/**
- * Just a simple enumeration of different application type currently supported by our engine.
- */
-enum class ApplicationType {
-    Windows, Android, WebGL
-}
