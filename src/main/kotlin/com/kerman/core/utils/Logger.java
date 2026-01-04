2388ac1f115b49f1842a7c055aa1e5d8ef11cf9a
hojat72elect <hojat72elect@gmail.com>
2026-01-04 17:45:46 -0500

    Logger.java

---
+++ src/main/kotlin/com/kerman/core/utils/Logger.java
@@ -0,0 +1,67 @@
+package com.kerman.core.utils;
+
+import com.kerman.core.Application;
+import com.kerman.core.Kerman;
+
+/**
+ * Info : This class was inspired by the "com.badlogic.gdx.utils.Logger".
+ * <p>
+ * Simple logger that uses the {@link Application} logging facilities to output messages. The log level set with
+ * {@link Application#setLogLevel(int)} overrides the log level set here.
+ */
+public class Logger {
+    static public final int NONE = 0;
+    static public final int ERROR = 1;
+    static public final int INFO = 2;
+    static public final int DEBUG = 3;
+
+    private final String tag;
+    private int level;
+
+    public Logger(String tag) {
+        this(tag, ERROR);
+    }
+
+    public Logger(String tag, int level) {
+        this.tag = tag;
+        this.level = level;
+    }
+
+    public void debug(String message) {
+        if (level >= DEBUG) Kerman.app.debug(tag, message);
+    }
+
+    public void debug(String message, Exception exception) {
+        if (level >= DEBUG) Kerman.app.debug(tag, message, exception);
+    }
+
+    public void info(String message) {
+        if (level >= INFO) Kerman.app.log(tag, message);
+    }
+
+    public void info(String message, Exception exception) {
+        if (level >= INFO) Kerman.app.log(tag, message, exception);
+    }
+
+    public void error(String message) {
+        if (level >= ERROR) Kerman.app.error(tag, message);
+    }
+
+    public void error(String message, Throwable exception) {
+        if (level >= ERROR) Kerman.app.error(tag, message, exception);
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    /**
+     * Sets the log level. {@link #NONE} will mute all log output. {@link #ERROR} will only let error messages through.
+     * {@link #INFO} will let all non-debug messages through, and {@link #DEBUG} will let all messages through.
+     *
+     * @param level {@link #NONE}, {@link #ERROR}, {@link #INFO}, {@link #DEBUG}.
+     */
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
