1638e83a34b96ba80ace70cba340ab2f6cc5aa15
hojat72elect <hojat72elect@gmail.com>
2026-01-03 19:56:27 -0500

    SoundLoader.java

---
+++ src/main/kotlin/com/kerman/core/assets/loaders/SoundLoader.java
@@ -0,0 +1,51 @@
+package com.kerman.core.assets.loaders;
+
+import com.kerman.core.Kerman;
+import com.kerman.core.assets.AssetDescriptor;
+import com.kerman.core.assets.AssetLoaderParameters;
+import com.kerman.core.assets.AssetManager;
+import com.kerman.core.audio.Sound;
+import com.kerman.core.files.FileHandle;
+import com.kerman.core.utils.KermanArray;
+
+/**
+ * Info : this class was inspired by "com.badlogic.gdx.assets.loaders.SoundLoader".
+ * <p>
+ * {@link AssetLoader} to load {@link Sound} instances.
+ */
+public class SoundLoader extends AsynchronousAssetLoader<Sound, SoundLoader.SoundParameter> {
+
+    private Sound sound;
+
+    public SoundLoader(FileHandleResolver resolver) {
+        super(resolver);
+    }
+
+    /**
+     * Returns the {@link Sound} instance currently loaded by this {@link SoundLoader}.
+     * @return the currently loaded {@link Sound}, otherwise {@code null} if no {@link Sound} has been loaded yet.
+     */
+    protected Sound getLoadedSound() {
+        return sound;
+    }
+
+    @Override
+    public void loadAsync(AssetManager manager, String fileName, FileHandle file, SoundParameter parameter) {
+        sound = Kerman.audio.newSound(file);
+    }
+
+    @Override
+    public Sound loadSync(AssetManager manager, String fileName, FileHandle file, SoundParameter parameter) {
+        Sound sound = this.sound;
+        this.sound = null;
+        return sound;
+    }
+
+    @Override
+    public KermanArray<AssetDescriptor> getDependencies(String fileName, FileHandle file, SoundParameter parameter) {
+        return null;
+    }
+
+    static public class SoundParameter extends AssetLoaderParameters<Sound> {
+    }
+}
