f2b379614a8568ea21ec49cf02c3efa934a276e2
hojat72elect <hojat72elect@gmail.com>
2026-01-04 00:18:13 -0500

    ModelMeshPart.java

---
+++ src/main/kotlin/com/kerman/core/graphics/k3d/model/data/ModelMeshPart.java
@@ -0,0 +1,10 @@
+package com.kerman.core.graphics.k3d.model.data;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.graphics.g3d.model.data.ModelMeshPart".
+ */
+public class ModelMeshPart {
+    public String id;
+    public short[] indices;
+    public int primitiveType;
+}
