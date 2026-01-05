2ea697091c086135341151f0e980f7f355233605
hojat72elect <hojat72elect@gmail.com>
2026-01-04 23:52:30 -0500

    PixmapTextureData.java

---
+++ src/main/kotlin/com/kerman/core/graphics/glutils/PixmapTextureData.java
@@ -0,0 +1,84 @@
+package com.kerman.core.graphics.glutils;
+
+import com.kerman.core.graphics.Pixmap;
+import com.kerman.core.graphics.Pixmap.Format;
+import com.kerman.core.graphics.TextureData;
+import com.kerman.core.utils.KermanRuntimeException;
+
+/**
+ * This class was inspired by "com.badlogic.gdx.graphics.glutils.PixmapTextureData".
+ */
+public class PixmapTextureData implements TextureData {
+    final Pixmap pixmap;
+    final Format format;
+    final boolean useMipMaps;
+    final boolean disposePixmap;
+    final boolean managed;
+
+    public PixmapTextureData(Pixmap pixmap, Format format, boolean useMipMaps, boolean disposePixmap) {
+        this(pixmap, format, useMipMaps, disposePixmap, false);
+    }
+
+    public PixmapTextureData(Pixmap pixmap, Format format, boolean useMipMaps, boolean disposePixmap, boolean managed) {
+        this.pixmap = pixmap;
+        this.format = format == null ? pixmap.getFormat() : format;
+        this.useMipMaps = useMipMaps;
+        this.disposePixmap = disposePixmap;
+        this.managed = managed;
+    }
+
+    @Override
+    public boolean disposePixmap() {
+        return disposePixmap;
+    }
+
+    @Override
+    public Pixmap consumePixmap() {
+        return pixmap;
+    }
+
+    @Override
+    public int getWidth() {
+        return pixmap.getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return pixmap.getHeight();
+    }
+
+    @Override
+    public Format getFormat() {
+        return format;
+    }
+
+    @Override
+    public boolean useMipMaps() {
+        return useMipMaps;
+    }
+
+    @Override
+    public boolean isManaged() {
+        return managed;
+    }
+
+    @Override
+    public TextureDataType getType() {
+        return TextureDataType.Pixmap;
+    }
+
+    @Override
+    public void consumeCustomData(int target) {
+        throw new KermanRuntimeException("This TextureData implementation does not upload data itself");
+    }
+
+    @Override
+    public boolean isPrepared() {
+        return true;
+    }
+
+    @Override
+    public void prepare() {
+        throw new KermanRuntimeException("prepare() must not be called on a PixmapTextureData instance as it is already prepared.");
+    }
+}
