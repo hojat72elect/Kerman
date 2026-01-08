41339342eba3fab7d2aebc5c8f9df1993d48fcce
hojat72elect <hojat72elect@gmail.com>
2026-01-08 00:48:14 -0500

    InputAdapter.java

---
+++ src/main/kotlin/com/kerman/core/InputAdapter.java
@@ -0,0 +1,46 @@
+package com.kerman.core;
+
+/**
+ * This class was inspired by "com.badlogic.gdx.InputAdapter".
+ * <p>
+ * An adapter class for {@link InputProcessor}. You can derive from this and only override what you are interested in.
+ */
+public class InputAdapter implements InputProcessor {
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+}
