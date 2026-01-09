28e622e2c330b06e78c2eca9e4878e1ef2756d03
hojat72elect <hojat72elect@gmail.com>
2026-01-09 00:07:16 -0500

    Touchable.java

---
+++ src/main/kotlin/com/kerman/core/scenes/scene2d/Touchable.java
@@ -0,0 +1,22 @@
+package com.kerman.core.scenes.scene2d;
+
+/**
+ * Info : This enum was inspired by "com.badlogic.gdx.scenes.scene2d.Touchable".
+ * <p>
+ * Determines how touch input events are distributed to an actor and any children.
+ */
+public enum Touchable {
+    /**
+     * All touch input events will be received by the actor and any children.
+     */
+    enabled,
+    /**
+     * No touch input events will be received by the actor or any children.
+     */
+    disabled,
+    /**
+     * No touch input events will be received by the actor, but children will still receive events. Note that events on the
+     * children will still bubble to the parent.
+     */
+    childrenOnly
+}
