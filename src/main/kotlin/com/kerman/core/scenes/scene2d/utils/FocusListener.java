14dd43cbc9de1fcd2fc2c6fada9a5fac0bd52a6e
hojat72elect <hojat72elect@gmail.com>
2026-01-11 23:31:32 -0500

    FocusListener.java

---
+++ src/main/kotlin/com/kerman/core/scenes/scene2d/utils/FocusListener.java
@@ -0,0 +1,91 @@
+package com.kerman.core.scenes.scene2d.utils;
+
+import com.kerman.core.scenes.scene2d.Actor;
+import com.kerman.core.scenes.scene2d.Event;
+import com.kerman.core.scenes.scene2d.EventListener;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Info : This abstract class was inspired by "com.badlogic.gdx.scenes.scene2d.utils.FocusListener".
+ *
+ * Listener for {@link FocusEvent}.
+ */
+abstract public class FocusListener implements EventListener {
+    public boolean handle(@NotNull Event event) {
+        if (!(event instanceof FocusEvent)) return false;
+        FocusEvent focusEvent = (FocusEvent) event;
+        switch (focusEvent.getType()) {
+            case keyboard:
+                keyboardFocusChanged(focusEvent, event.getTarget(), focusEvent.isFocused());
+                break;
+            case scroll:
+                scrollFocusChanged(focusEvent, event.getTarget(), focusEvent.isFocused());
+                break;
+        }
+        return false;
+    }
+
+    /**
+     * @param actor The event target, which is the actor that emitted the focus event.
+     */
+    public void keyboardFocusChanged(FocusEvent event, Actor actor, boolean focused) {
+    }
+
+    /**
+     * @param actor The event target, which is the actor that emitted the focus event.
+     */
+    public void scrollFocusChanged(FocusEvent event, Actor actor, boolean focused) {
+    }
+
+    /**
+     * Fired when an actor gains or loses keyboard or scroll focus. Can be cancelled to prevent losing or gaining focus.
+     */
+    static public class FocusEvent extends Event {
+        private boolean focused;
+        private Type type;
+        private Actor relatedActor;
+
+        public void reset() {
+            super.reset();
+            relatedActor = null;
+        }
+
+        public boolean isFocused() {
+            return focused;
+        }
+
+        public void setFocused(boolean focused) {
+            this.focused = focused;
+        }
+
+        public Type getType() {
+            return type;
+        }
+
+        public void setType(Type focusType) {
+            this.type = focusType;
+        }
+
+        /**
+         * The actor related to the event. When focus is lost, this is the new actor being focused, or null. When focus is gained,
+         * this is the previous actor that was focused, or null.
+         */
+        public @Nullable Actor getRelatedActor() {
+            return relatedActor;
+        }
+
+        /**
+         * @param relatedActor May be null.
+         */
+        public void setRelatedActor(@Nullable Actor relatedActor) {
+            this.relatedActor = relatedActor;
+        }
+
+        /**
+         *          */
+        public enum Type {
+            keyboard, scroll
+        }
+    }
+}
