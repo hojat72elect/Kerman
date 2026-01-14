3e0b449562aa83e020d365e10aa7a6b34ef69021
hojat72elect <hojat72elect@gmail.com>
2026-01-13 21:35:14 -0500

    Disableable.kt

---
+++ src/main/kotlin/com/kerman/core/scenes/scene2d/utils/Disableable.kt
@@ -0,0 +1,9 @@
+package com.kerman.core.scenes.scene2d.utils
+
+/**
+ * Info : This interface was inspired by "com.badlogic.gdx.scenes.scene2d.utils.Disableable".
+ */
+interface Disableable {
+    fun isDisabled(): Boolean
+    fun setDisabled(isDisabled: Boolean)
+}
