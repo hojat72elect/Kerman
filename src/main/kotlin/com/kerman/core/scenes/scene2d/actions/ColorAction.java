784c1600e33c93af1bf2833d8960c94a553a4ff2
hojat72elect <hojat72elect@gmail.com>
2026-01-14 21:30:26 -0500

    ColorAction.java

---
+++ src/main/kotlin/com/kerman/core/scenes/scene2d/actions/ColorAction.java
@@ -0,0 +1,67 @@
+package com.kerman.core.scenes.scene2d.actions;
+
+import com.kerman.core.graphics.Color;
+import com.kerman.core.scenes.scene2d.Actor;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.scenes.scene2d.actions.ColorAction".
+ * <p>
+ * Sets the actor's color (or a specified color), from the current to the new color. Note this action transitions from the color
+ * at the time the action starts to the specified color.
+ */
+public class ColorAction extends TemporalAction {
+    private final Color end = new Color();
+    private float startR, startG, startB, startA;
+    private @Nullable Color color;
+
+    protected void begin() {
+        if (color == null) color = target.getColor();
+        startR = color.r;
+        startG = color.g;
+        startB = color.b;
+        startA = color.a;
+    }
+
+    protected void update(float percent) {
+        if (percent == 0)
+            color.set(startR, startG, startB, startA);
+        else if (percent == 1)
+            color.set(end);
+        else {
+            float r = startR + (end.r - startR) * percent;
+            float g = startG + (end.g - startG) * percent;
+            float b = startB + (end.b - startB) * percent;
+            float a = startA + (end.a - startA) * percent;
+            color.set(r, g, b, a);
+        }
+    }
+
+    public void reset() {
+        super.reset();
+        color = null;
+    }
+
+    public @Nullable Color getColor() {
+        return color;
+    }
+
+    /**
+     * Sets the color to modify. If null (the default), the {@link #getActor() actor's} {@link Actor#getColor() color} will be
+     * used.
+     */
+    public void setColor(@Nullable Color color) {
+        this.color = color;
+    }
+
+    public Color getEndColor() {
+        return end;
+    }
+
+    /**
+     * Sets the color to transition to. Required.
+     */
+    public void setEndColor(Color color) {
+        end.set(color);
+    }
+}
