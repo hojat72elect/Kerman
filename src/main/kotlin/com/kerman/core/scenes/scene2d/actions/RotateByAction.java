ee9981793b5f5e9968fcfa81a7f2aad357344aaf
hojat72elect <hojat72elect@gmail.com>
2026-01-15 01:04:06 -0500

    RotateByAction.java

---
+++ src/main/kotlin/com/kerman/core/scenes/scene2d/actions/RotateByAction.java
@@ -0,0 +1,22 @@
+package com.kerman.core.scenes.scene2d.actions;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.scenes.scene2d.actions.RotateByAction".
+ * <p>
+ * Sets the actor's rotation from its current value to a relative value.
+ */
+public class RotateByAction extends RelativeTemporalAction {
+    private float amount;
+
+    protected void updateRelative(float percentDelta) {
+        target.rotateBy(amount * percentDelta);
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float rotationAmount) {
+        amount = rotationAmount;
+    }
+}
