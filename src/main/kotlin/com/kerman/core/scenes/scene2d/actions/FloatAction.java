a844c1e5fda2eab7a60feb375e00d29aed1cf852
hojat72elect <hojat72elect@gmail.com>
2026-01-14 22:26:41 -0500

    FloatAction.java

---
+++ src/main/kotlin/com/kerman/core/scenes/scene2d/actions/FloatAction.java
@@ -0,0 +1,98 @@
+package com.kerman.core.scenes.scene2d.actions;
+
+import com.kerman.core.math.Interpolation;
+
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.scenes.scene2d.actions.FloatAction".
+ * <p>
+ * An action that has a float, whose value is transitioned over time.
+ */
+public class FloatAction extends TemporalAction {
+    private float start, end;
+    private float value;
+
+    /**
+     * Creates a FloatAction that transitions from 0 to 1.
+     */
+    public FloatAction() {
+        start = 0;
+        end = 1;
+    }
+
+    /**
+     * Creates a FloatAction that transitions from start to end.
+     */
+    public FloatAction(float start, float end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     * Creates a FloatAction that transitions from start to end.
+     */
+    public FloatAction(float start, float end, float duration) {
+        super(duration);
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     * Creates a FloatAction that transitions from start to end.
+     */
+    public FloatAction(float start, float end, float duration, @Nullable Interpolation interpolation) {
+        super(duration, interpolation);
+        this.start = start;
+        this.end = end;
+    }
+
+    protected void begin() {
+        value = start;
+    }
+
+    protected void update(float percent) {
+        if (percent == 0)
+            value = start;
+        else if (percent == 1)
+            value = end;
+        else
+            value = start + (end - start) * percent;
+    }
+
+    /**
+     * Gets the current float value.
+     */
+    public float getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the current float value.
+     */
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public float getStart() {
+        return start;
+    }
+
+    /**
+     * Sets the value to transition from.
+     */
+    public void setStart(float start) {
+        this.start = start;
+    }
+
+    public float getEnd() {
+        return end;
+    }
+
+    /**
+     * Sets the value to transition to.
+     */
+    public void setEnd(float end) {
+        this.end = end;
+    }
+}
