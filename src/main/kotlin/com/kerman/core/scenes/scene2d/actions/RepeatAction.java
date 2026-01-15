bb536fc870fb9a67694d7bc47459dff2f5e7e40e
hojat72elect <hojat72elect@gmail.com>
2026-01-14 23:10:30 -0500

    RepeatAction.java

---
+++ src/main/kotlin/com/kerman/core/scenes/scene2d/actions/RepeatAction.java
@@ -0,0 +1,48 @@
+package com.kerman.core.scenes.scene2d.actions;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.scenes.scene2d.actions.RepeatAction".
+ * <p>
+ * Repeats an action a number of times or forever.
+ */
+public class RepeatAction extends DelegateAction implements FinishableAction {
+    static public final int FOREVER = -1;
+
+    private int repeatCount, executedCount;
+    private boolean finished;
+
+    protected boolean delegate(float delta) {
+        if (executedCount == repeatCount) return true;
+        if (action.act(delta)) {
+            if (finished) return true;
+            if (repeatCount > 0) executedCount++;
+            if (executedCount == repeatCount) return true;
+            if (action != null) action.restart();
+        }
+        return false;
+    }
+
+    /**
+     * Causes the action to not repeat again.
+     */
+    public void finish() {
+        finished = true;
+    }
+
+    public void restart() {
+        super.restart();
+        executedCount = 0;
+        finished = false;
+    }
+
+    public int getCount() {
+        return repeatCount;
+    }
+
+    /**
+     * Sets the number of times to repeat. Can be set to {@link #FOREVER}.
+     */
+    public void setCount(int count) {
+        this.repeatCount = count;
+    }
+}
