4e685b5312d62b563ed34be85958ccaab5080090
hojat72elect <hojat72elect@gmail.com>
2026-01-15 01:21:29 -0500

    SizeToAction.java

---
+++ src/main/kotlin/com/kerman/core/scenes/scene2d/actions/SizeToAction.java
@@ -0,0 +1,52 @@
+package com.kerman.core.scenes.scene2d.actions;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.scenes.scene2d.actions.SizeToAction".
+ * <p>
+ * Moves an actor from its current size to a specific size.
+ */
+public class SizeToAction extends TemporalAction {
+    private float startWidth, startHeight;
+    private float endWidth, endHeight;
+
+    protected void begin() {
+        startWidth = target.getWidth();
+        startHeight = target.getHeight();
+    }
+
+    protected void update(float percent) {
+        float width, height;
+        if (percent == 0) {
+            width = startWidth;
+            height = startHeight;
+        } else if (percent == 1) {
+            width = endWidth;
+            height = endHeight;
+        } else {
+            width = startWidth + (endWidth - startWidth) * percent;
+            height = startHeight + (endHeight - startHeight) * percent;
+        }
+        target.setSize(width, height);
+    }
+
+    public void setSize(float width, float height) {
+        endWidth = width;
+        endHeight = height;
+    }
+
+    public float getWidth() {
+        return endWidth;
+    }
+
+    public void setWidth(float width) {
+        endWidth = width;
+    }
+
+    public float getHeight() {
+        return endHeight;
+    }
+
+    public void setHeight(float height) {
+        endHeight = height;
+    }
+}
