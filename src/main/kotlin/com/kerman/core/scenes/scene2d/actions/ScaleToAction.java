2e08d5f962ccafb9cc0f29691c79fa9bf18a2ea7
hojat72elect <hojat72elect@gmail.com>
2026-01-15 01:08:39 -0500

    ScaleToAction.java

---
+++ src/main/kotlin/com/kerman/core/scenes/scene2d/actions/ScaleToAction.java
@@ -0,0 +1,57 @@
+package com.kerman.core.scenes.scene2d.actions;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction".
+ * <p>
+ * Sets the actor's scale from its current value to a specific value.
+ */
+public class ScaleToAction extends TemporalAction {
+    private float startX, startY;
+    private float endX, endY;
+
+    protected void begin() {
+        startX = target.getScaleX();
+        startY = target.getScaleY();
+    }
+
+    protected void update(float percent) {
+        float x, y;
+        if (percent == 0) {
+            x = startX;
+            y = startY;
+        } else if (percent == 1) {
+            x = endX;
+            y = endY;
+        } else {
+            x = startX + (endX - startX) * percent;
+            y = startY + (endY - startY) * percent;
+        }
+        target.setScale(x, y);
+    }
+
+    public void setScale(float x, float y) {
+        endX = x;
+        endY = y;
+    }
+
+    public void setScale(float scale) {
+        endX = scale;
+        endY = scale;
+    }
+
+    public float getX() {
+        return endX;
+    }
+
+    public void setX(float x) {
+        this.endX = x;
+    }
+
+    public float getY() {
+        return endY;
+    }
+
+    public void setY(float y) {
+        this.endY = y;
+    }
+}
