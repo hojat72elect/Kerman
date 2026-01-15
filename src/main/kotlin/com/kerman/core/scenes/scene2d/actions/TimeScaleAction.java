2a2d4ca90e68d75d8909f30137154c3ffb5cb06e
hojat72elect <hojat72elect@gmail.com>
2026-01-15 11:37:11 -0500

    TimeScaleAction.java

---
+++ src/main/kotlin/com/kerman/core/scenes/scene2d/actions/TimeScaleAction.java
@@ -0,0 +1,23 @@
+package com.kerman.core.scenes.scene2d.actions;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.scenes.scene2d.actions.TimeScaleAction".
+ * <p>
+ * Multiplies the delta of an action.
+ */
+public class TimeScaleAction extends DelegateAction {
+    private float scale;
+
+    protected boolean delegate(float delta) {
+        if (action == null) return true;
+        return action.act(delta * scale);
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
+}
