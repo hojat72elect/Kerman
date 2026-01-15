2d0e73511bd4250c253cc525f6a7eef58af5c5c5
hojat72elect <hojat72elect@gmail.com>
2026-01-14 22:19:57 -0500

    RemoveListenerAction.java

---
+++ src/main/kotlin/com/kerman/core/scenes/scene2d/actions/RemoveListenerAction.java
@@ -0,0 +1,43 @@
+package com.kerman.core.scenes.scene2d.actions;
+
+import com.kerman.core.scenes.scene2d.Action;
+import com.kerman.core.scenes.scene2d.EventListener;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.scenes.scene2d.actions.RemoveListenerAction".
+ * <p>
+ * Removes a listener from an actor.
+ */
+public class RemoveListenerAction extends Action {
+    private EventListener listener;
+    private boolean capture;
+
+    public boolean act(float delta) {
+        if (capture)
+            target.removeCaptureListener(listener);
+        else
+            target.removeListener(listener);
+        return true;
+    }
+
+    public EventListener getListener() {
+        return listener;
+    }
+
+    public void setListener(EventListener listener) {
+        this.listener = listener;
+    }
+
+    public boolean getCapture() {
+        return capture;
+    }
+
+    public void setCapture(boolean capture) {
+        this.capture = capture;
+    }
+
+    public void reset() {
+        super.reset();
+        listener = null;
+    }
+}
