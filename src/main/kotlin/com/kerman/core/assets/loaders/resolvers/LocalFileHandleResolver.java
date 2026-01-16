883cba5ca559002ce9a4201092bcab26f9517681
hojat72elect <hojat72elect@gmail.com>
2026-01-16 16:12:40 -0500

    LocalFileHandleResolver.java

---
+++ src/main/kotlin/com/kerman/core/assets/loaders/resolvers/LocalFileHandleResolver.java
@@ -0,0 +1,15 @@
+package com.kerman.core.assets.loaders.resolvers;
+
+import com.kerman.core.Kerman;
+import com.kerman.core.assets.loaders.FileHandleResolver;
+import com.kerman.core.files.FileHandle;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.assets.loaders.resolvers.LocalFileHandleResolver".
+ */
+public class LocalFileHandleResolver implements FileHandleResolver {
+    @Override
+    public FileHandle resolve(String fileName) {
+        return Kerman.files.local(fileName);
+    }
+}
