37da8fdcdac69760eeb2d5c4f467fdff915fdb7a
hojat72elect <hojat72elect@gmail.com>
2026-01-16 11:40:39 -0500

    TiledMapTileSets.java

---
+++ src/main/kotlin/com/kerman/core/maps/tiled/TiledMapTileSets.java
@@ -0,0 +1,94 @@
+package com.kerman.core.maps.tiled;
+
+import com.kerman.core.utils.KermanArray;
+import java.util.Iterator;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.maps.tiled.TiledMapTileSets".
+ * <p>
+ * Collection of {@link TiledMapTileSet}
+ */
+public class TiledMapTileSets implements Iterable<TiledMapTileSet> {
+
+    private final KermanArray<TiledMapTileSet> tilesets;
+
+    /**
+     * Creates an empty collection of tilesets.
+     */
+    public TiledMapTileSets() {
+        tilesets = new KermanArray<TiledMapTileSet>();
+    }
+
+    /**
+     * @param index index to get the desired {@link TiledMapTileSet} at.
+     * @return tileset at index
+     */
+    public TiledMapTileSet getTileSet(int index) {
+        return tilesets.get(index);
+    }
+
+    /**
+     * @param name Name of the {@link TiledMapTileSet} to retrieve.
+     * @return tileset with matching name, null if it doesn't exist
+     */
+    public TiledMapTileSet getTileSet(String name) {
+        for (TiledMapTileSet tileset : tilesets) {
+            if (name.equals(tileset.getName())) {
+                return tileset;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param tileset set to be added to the collection
+     */
+    public void addTileSet(TiledMapTileSet tileset) {
+        tilesets.add(tileset);
+    }
+
+    /**
+     * Removes tileset at index
+     *
+     * @param index index at which to remove a tileset.
+     */
+    public void removeTileSet(int index) {
+        tilesets.removeIndex(index);
+    }
+
+    /**
+     * @param tileset set to be removed
+     */
+    public void removeTileSet(TiledMapTileSet tileset) {
+        tilesets.removeValue(tileset, true);
+    }
+
+    /**
+     * @param id id of the {@link TiledMapTile} to get.
+     * @return tile with matching id, null if it doesn't exist
+     */
+    public TiledMapTile getTile(int id) {
+        // The purpose of backward iteration here is to maintain backwards compatibility
+        // with maps created with earlier versions of a shared tileset. The assumption
+        // is that the tilesets are in order of ascending firstgid, and by backward
+        // iterating precedence for conflicts is given to later tilesets in the list,
+        // which are likely to be the earlier version of any given gid.
+        // See TiledMapModifiedExternalTilesetTest for example of this issue.
+        for (int i = tilesets.size - 1; i >= 0; i--) {
+            TiledMapTileSet tileset = tilesets.get(i);
+            TiledMapTile tile = tileset.getTile(id);
+            if (tile != null) {
+                return tile;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @return iterator to tilesets
+     */
+    @Override
+    public Iterator<TiledMapTileSet> iterator() {
+        return tilesets.iterator();
+    }
+}
