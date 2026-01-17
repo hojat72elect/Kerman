ae97c81f28829e2f9ab784fbbe02fe7e444793a5
hojat72elect <hojat72elect@gmail.com>
2026-01-17 06:45:59 -0500

    MapGroupLayer.java

---
+++ src/main/kotlin/com/kerman/core/maps/MapGroupLayer.java
@@ -0,0 +1,27 @@
+package com.kerman.core.maps;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.maps.MapGroupLayer".
+ * <p>
+ * Map layer containing a set of MapLayers, objects and properties.
+ */
+public class MapGroupLayer extends MapLayer {
+
+    private final MapLayers layers = new MapLayers();
+
+    /**
+     * @return the {@link MapLayers} owned by this group
+     */
+    public MapLayers getLayers() {
+        return layers;
+    }
+
+    @Override
+    public void invalidateRenderOffset() {
+        super.invalidateRenderOffset();
+        for (int i = 0; i < layers.size(); i++) {
+            MapLayer child = layers.get(i);
+            child.invalidateRenderOffset();
+        }
+    }
+}
