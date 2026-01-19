0fa5cc28153a82b2c515496a57917e618b1520e2
hojat72elect <hojat72elect@gmail.com>
2026-01-18 22:15:14 -0500

    ModelMesh.java

---
+++ src/main/kotlin/com/kerman/core/graphics/k3d/model/data/ModelMesh.java
@@ -0,0 +1,13 @@
+package com.kerman.core.graphics.k3d.model.data;
+
+import com.kerman.core.graphics.VertexAttribute;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.graphics.g3d.model.data.ModelMesh".
+ */
+public class ModelMesh {
+    public String id;
+    public VertexAttribute[] attributes;
+    public float[] vertices;
+    public ModelMeshPart[] parts;
+}
