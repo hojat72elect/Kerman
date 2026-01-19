9d2535b16e8b6003a15a9f5bd11c7653cf3b17e1
hojat72elect <hojat72elect@gmail.com>
2026-01-19 00:19:26 -0500

    FrameBuffer.java

---
+++ src/main/kotlin/com/kerman/core/graphics/glutils/FrameBuffer.java
@@ -0,0 +1,98 @@
+package com.kerman.core.graphics.glutils;
+
+import com.kerman.core.Application;
+import com.kerman.core.Kerman;
+import com.kerman.core.graphics.GLES20;
+import com.kerman.core.graphics.Pixmap;
+import com.kerman.core.graphics.Texture;
+import com.kerman.core.graphics.Texture.TextureFilter;
+import com.kerman.core.graphics.Texture.TextureWrap;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.graphics.glutils.FrameBuffer".
+ * <p>
+ * Encapsulates OpenGL ES 2.0 frame buffer objects. This is a simple helper class which should cover most FBO uses. It will
+ * automatically create a texture for the color attachment and a renderbuffer for the depth buffer. You can get a hold of the
+ * texture by {@link FrameBuffer#getColorBufferTexture()}. This class will only work with OpenGL ES 2.0.
+ * </p>
+ *
+ * <p>
+ * FrameBuffers are managed. In case of an OpenGL context loss, which only happens on Android when a user switches to another
+ * application or receives an incoming call, the framebuffer will be automatically recreated.
+ * </p>
+ *
+ * <p>
+ * A FrameBuffer must be disposed if it is no longer needed
+ * </p>
+ */
+public class FrameBuffer extends GLFrameBuffer<Texture> {
+
+    FrameBuffer() {
+    }
+
+    /**
+     * Creates a GLFrameBuffer from the specifications provided by bufferBuilder
+     **/
+    protected FrameBuffer(GLFrameBufferBuilder<? extends GLFrameBuffer<Texture>> bufferBuilder) {
+        super(bufferBuilder);
+    }
+
+    /**
+     * Creates a new FrameBuffer having the given dimensions and potentially a depth buffer attached.
+     */
+    public FrameBuffer(Pixmap.Format format, int width, int height, boolean hasDepth) {
+        this(format, width, height, hasDepth, false);
+    }
+
+    /**
+     * Creates a new FrameBuffer having the given dimensions and potentially a depth and a stencil buffer attached.
+     *
+     * @param format   the format of the color buffer; according to the OpenGL ES 2.0 spec, only RGB565, RGBA4444 and RGB5_A1 are
+     *                 color-renderable
+     * @param width    the width of the framebuffer in pixels
+     * @param height   the height of the framebuffer in pixels
+     * @param hasDepth whether to attach a depth buffer
+     * @throws com.kerman.core.utils.KermanRuntimeException in case the FrameBuffer could not be created
+     */
+    public FrameBuffer(Pixmap.Format format, int width, int height, boolean hasDepth, boolean hasStencil) {
+        FrameBufferBuilder frameBufferBuilder = new FrameBufferBuilder(width, height);
+        frameBufferBuilder.addBasicColorTextureAttachment(format);
+        if (hasDepth) frameBufferBuilder.addBasicDepthRenderBuffer();
+        if (hasStencil) frameBufferBuilder.addBasicStencilRenderBuffer();
+        this.bufferBuilder = frameBufferBuilder;
+
+        build();
+    }
+
+    /**
+     * See {@link GLFrameBuffer#unbind()}
+     */
+    public static void unbind() {
+        GLFrameBuffer.unbind();
+    }
+
+    @Override
+    protected Texture createTexture(FrameBufferTextureAttachmentSpec attachmentSpec) {
+        GLOnlyTextureData data = new GLOnlyTextureData(bufferBuilder.width, bufferBuilder.height, 0, attachmentSpec.internalFormat,
+                attachmentSpec.format, attachmentSpec.type);
+        Texture result = new Texture(data);
+        // Filtering support for depth textures on WebGL is spotty https://github.com/KhronosGroup/OpenGL-API/issues/84
+        boolean webGLDepth = attachmentSpec.isDepth && Kerman.app.getType() == Application.ApplicationType.WebGL;
+        if (!webGLDepth) {
+            result.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+        }
+        result.setWrap(TextureWrap.ClampToEdge, TextureWrap.ClampToEdge);
+        return result;
+    }
+
+    @Override
+    protected void disposeColorTexture(Texture colorTexture) {
+        colorTexture.dispose();
+    }
+
+    @Override
+    protected void attachFrameBufferColorTexture(Texture texture) {
+        Kerman.gl20.glFramebufferTexture2D(GLES20.GL_FRAMEBUFFER, GLES20.GL_COLOR_ATTACHMENT0, GLES20.GL_TEXTURE_2D,
+                texture.getTextureObjectHandle(), 0);
+    }
+}
