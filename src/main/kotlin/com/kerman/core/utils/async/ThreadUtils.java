619eb59bc33d7350ce9582230f0fa4964712d882
hojat72elect <hojat72elect@gmail.com>
2026-01-18 23:42:55 -0500

    ThreadUtils.java

---
+++ src/main/kotlin/com/kerman/core/utils/async/ThreadUtils.java
@@ -0,0 +1,12 @@
+package com.kerman.core.utils.async;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.utils.async.ThreadUtils".
+ * <p>
+ * Utilities for threaded programming.
+ */
+public class ThreadUtils {
+    public static void yield() {
+        Thread.yield();
+    }
+}
