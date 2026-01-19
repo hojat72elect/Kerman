6e70e124b201a7af5e8112cbfdb5b2b3c251529d
hojat72elect <hojat72elect@gmail.com>
2026-01-18 23:47:18 -0500

    Cursor.kt

--- src/main/kotlin/com/kerman/core/graphics/Cursor.kt
+++ src/main/kotlin/com/kerman/core/graphics/Cursor.kt
@@ -1,6 +1,16 @@
 package com.kerman.core.graphics
 
-interface Cursor {
-}
+import com.kerman.core.Graphics
+import com.kerman.core.utils.Disposable
 
-enum class SystemCursor{}
+/**
+ * Info : This interface was inspired by "com.badlogic.gdx.graphics.Cursor".
+ *
+ * Represents a mouse cursor. Create a cursor via [Graphics.newCursor]. To set the cursor use
+ * [Graphics.setCursor]. To use one of the system cursors, call [Graphics.setSystemCursor]
+ */
+interface Cursor : Disposable {
+    enum class SystemCursor {
+        Arrow, Ibeam, Crosshair, Hand, HorizontalResize, VerticalResize, NWSEResize, NESWResize, AllResize, NotAllowed, None
+    }
+}
