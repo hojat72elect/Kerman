93913ae89785dcba880d9d3532211486620184d3
hojat72elect <hojat72elect@gmail.com>
2026-01-20 11:41:05 -0500

    Version.java

---
+++ src/main/kotlin/com/kerman/core/Version.java
@@ -0,0 +1,62 @@
+package com.kerman.core;
+
+import com.kerman.core.utils.KermanRuntimeException;
+
+/**
+ * Info : This class was inspired by "com.badlogic.gdx.Version".
+ * <p>
+ * The version of libGDX
+ */
+public class Version {
+    /**
+     * The current version of libGDX as a String in the major.minor.revision format
+     **/
+    public static final String VERSION = "1.14.1";
+
+    /**
+     * The current major version of libGDX
+     **/
+    public static final int MAJOR;
+
+    /**
+     * The current minor version of libGDX
+     **/
+    public static final int MINOR;
+
+    /**
+     * The current revision version of libGDX
+     **/
+    public static final int REVISION;
+
+    static {
+        try {
+            String[] v = VERSION.split("\\.");
+            MAJOR = v.length < 1 ? 0 : Integer.valueOf(v[0]);
+            MINOR = v.length < 2 ? 0 : Integer.valueOf(v[1]);
+            REVISION = v.length < 3 ? 0 : Integer.valueOf(v[2]);
+        } catch (Throwable t) {
+            // Should never happen
+            throw new KermanRuntimeException("Invalid version " + VERSION, t);
+        }
+    }
+
+    public static boolean isHigher(int major, int minor, int revision) {
+        return isHigherEqual(major, minor, revision + 1);
+    }
+
+    public static boolean isHigherEqual(int major, int minor, int revision) {
+        if (MAJOR != major) return MAJOR > major;
+        if (MINOR != minor) return MINOR > minor;
+        return REVISION >= revision;
+    }
+
+    public static boolean isLower(int major, int minor, int revision) {
+        return isLowerEqual(major, minor, revision - 1);
+    }
+
+    public static boolean isLowerEqual(int major, int minor, int revision) {
+        if (MAJOR != major) return MAJOR < major;
+        if (MINOR != minor) return MINOR < minor;
+        return REVISION <= revision;
+    }
+}
