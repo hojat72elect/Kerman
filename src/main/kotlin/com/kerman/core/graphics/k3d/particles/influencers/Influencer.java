df14244c34ec362c0160957dd1801eceab5a807f
hojat72elect <hojat72elect@gmail.com>
2026-01-19 23:48:41 -0500

    Influencer.java

---
+++ src/main/kotlin/com/kerman/core/graphics/k3d/particles/influencers/Influencer.java
@@ -0,0 +1,13 @@
+package com.kerman.core.graphics.k3d.particles.influencers;
+
+import com.kerman.core.graphics.k3d.particles.ParticleControllerComponent;
+
+/**
+ * Info : This abstract class was inspired by "com.badlogic.gdx.graphics.g3d.particles.influencers.Influencer".
+ * <p>
+ * It's a {@link ParticleControllerComponent} which usually modifies one or more properties of the particles(i.e color, scale,
+ * graphical representation, velocity, etc...).
+ */
+public abstract class Influencer extends ParticleControllerComponent {
+
+}
