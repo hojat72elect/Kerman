48ad5736d98a6db29ac3ca10a128b8b3d0e06c5e
hojat72elect <hojat72elect@gmail.com>
2026-01-20 02:13:57 -0500

    ParticleControllerRenderer.java

---
+++ src/main/kotlin/com/kerman/core/graphics/k3d/particles/renderers/ParticleControllerRenderer.java
@@ -0,0 +1,46 @@
+package com.kerman.core.graphics.k3d.particles.renderers;
+
+import com.kerman.core.graphics.k3d.particles.ParticleController;
+import com.kerman.core.graphics.k3d.particles.ParticleControllerComponent;
+import com.kerman.core.graphics.k3d.particles.batches.ParticleBatch;
+
+/**
+ * Info : This abstract class was inspired by "com.badlogic.gdx.graphics.g3d.particles.renderers.ParticleControllerRenderer".
+ * <p>
+ * It's a {@link ParticleControllerComponent} which determines how the particles are rendered. It's the base class of every
+ * particle renderer.
+ */
+public abstract class ParticleControllerRenderer<D extends ParticleControllerRenderData, T extends ParticleBatch<D>>
+        extends ParticleControllerComponent {
+    protected T batch;
+    protected D renderData;
+
+    protected ParticleControllerRenderer() {
+    }
+
+    protected ParticleControllerRenderer(D renderData) {
+        this.renderData = renderData;
+    }
+
+    @Override
+    public void update() {
+        batch.draw(renderData);
+    }
+
+    @SuppressWarnings("unchecked")
+    public boolean setBatch(ParticleBatch<?> batch) {
+        if (isCompatible(batch)) {
+            this.batch = (T) batch;
+            return true;
+        }
+        return false;
+    }
+
+    public abstract boolean isCompatible(ParticleBatch<?> batch);
+
+    @Override
+    public void set(ParticleController particleController) {
+        super.set(particleController);
+        if (renderData != null) renderData.controller = controller;
+    }
+}
