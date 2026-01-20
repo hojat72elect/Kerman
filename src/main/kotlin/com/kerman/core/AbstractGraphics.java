985675db3bf8cc7b1df7f01760d646ee6d9cb087
hojat72elect <hojat72elect@gmail.com>
2026-01-20 14:33:25 -0500

    AbstractGraphics.java GridPoint2.java

---
+++ src/main/kotlin/com/kerman/core/AbstractGraphics.java
@@ -0,0 +1,20 @@
+package com.kerman.core;
+
+public abstract class AbstractGraphics implements Graphics {
+
+    @Override
+    public float getRawDeltaTime() {
+        return getDeltaTime();
+    }
+
+    @Override
+    public float getDensity() {
+        float ppiX = getPpiX();
+        return (ppiX > 0 && ppiX <= Float.MAX_VALUE) ? ppiX / 160f : 1f;
+    }
+
+    @Override
+    public float getBackBufferScale() {
+        return getBackBufferWidth() / (float) getWidth();
+    }
+}
