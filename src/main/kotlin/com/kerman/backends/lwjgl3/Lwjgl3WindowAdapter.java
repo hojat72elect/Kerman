5c8aa8b05c2b93981cc04fca43ca454213105ca6
hojat72elect <hojat72elect@gmail.com>
2026-01-20 14:04:26 -0500

    Lwjgl3WindowAdapter.java

---
+++ src/main/kotlin/com/kerman/backends/lwjgl3/Lwjgl3WindowAdapter.java
@@ -0,0 +1,42 @@
+package com.kerman.backends.lwjgl3;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Convenience implementation of {@link Lwjgl3WindowListener}. Derive from this class and only overwrite the methods you are
+ * interested in.
+ */
+public class Lwjgl3WindowAdapter implements Lwjgl3WindowListener {
+    @Override
+    public void created(@NotNull Lwjgl3Window window) {
+    }
+
+    @Override
+    public void iconified(boolean isIconified) {
+    }
+
+    @Override
+    public void maximized(boolean isMaximized) {
+    }
+
+    @Override
+    public void focusLost() {
+    }
+
+    @Override
+    public void focusGained() {
+    }
+
+    @Override
+    public boolean closeRequested() {
+        return true;
+    }
+
+    @Override
+    public void filesDropped(@NotNull String[] files) {
+    }
+
+    @Override
+    public void refreshRequested() {
+    }
+}
