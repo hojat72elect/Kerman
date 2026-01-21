64987092957ce242755fe99a2fa924dcb5e4c390
hojat72elect <hojat72elect@gmail.com>
2026-01-20 19:37:47 -0500

    Added "jlayer" gradle dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,11 +15,14 @@ repositories {
 
 dependencies {
 
+    // LWJGL libs
     implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
     implementation("org.lwjgl:lwjgl")
     implementation("org.lwjgl:lwjgl-opengl")
     implementation("org.lwjgl:lwjgl-glfw")
 
+    // jlayer
+    implementation("com.badlogicgames.jlayer:jlayer:1.0.1-gdx")
 
     // for loading native libraries of different platforms.
     implementation("com.badlogicgames.gdx:gdx-jnigen-loader:2.5.0")
